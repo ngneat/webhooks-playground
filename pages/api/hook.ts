65dda97d2d21a93411152aa63adc48383b3c4c87
Netanel Basal <netanel7799@gmail.com>
2023-12-12 19:25:18 +0200

    init

--- pages/api/hook.ts
+++ pages/api/hook.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.method);
+
   res.status(200).json({ text: "Hello" });
 }
